0d3888848af9ef2827875560cbef8dd2c13290c4
Grizzly47 <106337263+Grizzly47@users.noreply.github.com>
2022-12-07 15:54:30 -0800

    Updated Code with Joe

--- Assets/ScriptableObjects/Panels/Panel.cs
+++ Assets/ScriptableObjects/Panels/Panel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Panel", menuName = "Panel")]
 public class Panel : ScriptableObject
